979a81914dd197d7e5c9ab9cf43b5099c05ac254
Ronald <ronaldne69@gmail.com>
2023-11-09 15:37:29 -0400

    the winner func add, restart button is working. 4

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,23 @@ function Restart({onClicked}){
 }
 
 export default function Board() {
-  const [xgame, StateXgame] = useState(true);
+  const [xgame, setXgame] = useState(true);
   const [square, setSquare] = useState(Array(9).fill(null));
 
   function handleClik(i){
     
-    const nextSquare = square.slice();
-    if (square[i]) return;
-    nextSquare[i] = (xgame) ? 'X': 'O';
-    StateXgame(!xgame);
-    setSquare(nextSquare);
+    if (square[i] || calculateWinner(square)) return;
+    square[i] = (xgame) ? 'X': 'O';
+    setXgame(!xgame);
+    setSquare(square);
   }
 
   function onClickRestar(){
     if (square.length > 0){
-      setSquare(square.length = 0); 
-      setSquare(square.fill(null));
+      setSquare(Array(9).fill(null));
     }
   }
+
 
   return(
     <>
@@ -52,8 +51,26 @@ export default function Board() {
         <Restart onClicked ={onClickRestar} />
       </div>
 
-
     </>
   ) 
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
