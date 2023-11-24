2a7ca3f98787e31fad5d9364f6921dac92a3b09e
PrRonald <ronaldne69@gmail.com>
2023-11-23 21:07:44 -0400

    the styles was update, I working in responsive design

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -39,53 +39,58 @@ function Board() {
   return(
     <>
     <div className="container">
-      <div className="container-square">
-        <div className="square-container square-item-0">
-          <Square clas={"square-0 square"} value={square[0]} onClicked={() => handleClik(0)}  /> 
-        </div>
-        <div className="square-container square-item-1">
-          <Square clas={"square-1 square"} value={square[1]} onClicked={() => handleClik(1)}  />
-        </div>
-        <div className="square-container square-item-2">
-          <Square clas={"square-2 square"} value={square[2]} onClicked={() => handleClik(2)}  />
-        </div>
-        <div className="square-container square-item-3">
-          <Square clas={"square-3 square"} value={square[3]} onClicked={() => handleClik(3)}/>
-        </div>
-        <div className="square-container square-item-4">
-          <Square clas={"square-4 square"} value={square[4]} onClicked={() => handleClik(4)}/>
-        </div>
-        <div className="square-container square-item-5">
-          <Square clas={"square-5 square"} value={square[5]} onClicked={() => handleClik(5)}  />
-        </div>
-        <div className="square-container square-item-6">
-          <Square clas={"square-6 square"} value={square[6]} onClicked={() => handleClik(6)}/>
-        </div>
-        <div className="square-container square-item-7">
-          <Square clas={"square-7 square"} value={square[7]} onClicked={() => handleClik(7)}/>
-        </div>
-        <div className="square-container square-item-8">
-          <Square clas={"square-8 square"} value={square[8]} onClicked={() => handleClik(8)}/>
-        </div>
 
-      </div>
       <div className="container-button">
-        <section className="container-nav-button">
-          <div className="nav-button">
-            <div className="nav-button-Undo">
+        <div className="container-nav-button">
+
+            <div className="nav-button-Undo button-cont">
               <Undo/>
             </div>
-            <div className="nav-button-Restar">
+
+            <div className="nav-button-Restar button-cont">
               <Restart/>
             </div>
-            <div className="nav-button-Redo">
+
+            <div className="nav-button-Redo button-cont">
               <Redo/>
             </div>
-          </div>
-        </section>
+
+        </div>
 
         <div className="history-button">
            <History/>
+        </div>
+      </div>
+      
+      <div className="container-square">
+        <div className="square-container-items">
+          <div className="square-item square-item-0">
+            <Square clas={"square-0 square"} value={square[0]} onClicked={() => handleClik(0)}  /> 
+          </div>
+          <div className="square-item square-item-1">
+            <Square clas={"square-1 square"} value={square[1]} onClicked={() => handleClik(1)}  />
+          </div>
+          <div className="square-item square-item-2">
+            <Square clas={"square-2 square"} value={square[2]} onClicked={() => handleClik(2)}  />
+          </div>
+          <div className="square-item square-item-3">
+            <Square clas={"square-3 square"} value={square[3]} onClicked={() => handleClik(3)}/>
+          </div>
+          <div className="square-item square-item-4">
+            <Square clas={"square-4 square"} value={square[4]} onClicked={() => handleClik(4)}/>
+          </div>
+          <div className="square-item square-item-5">
+            <Square clas={"square-5 square"} value={square[5]} onClicked={() => handleClik(5)}  />
+          </div>
+          <div className="square-item square-item-6">
+            <Square clas={"square-6 square"} value={square[6]} onClicked={() => handleClik(6)}/>
+          </div>
+          <div className="square-item square-item-7">
+            <Square clas={"square-7 square"} value={square[7]} onClicked={() => handleClik(7)}/>
+          </div>
+          <div className="square-item square-item-8">
+            <Square clas={"square-8 square"} value={square[8]} onClicked={() => handleClik(8)}/>
+          </div>
         </div>
       </div>
     </div>
